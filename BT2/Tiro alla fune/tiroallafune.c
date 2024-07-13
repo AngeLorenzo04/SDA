bccd0bea55a0694c2253631b05c26a7bdfa61886
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2024-07-13 21:00:18 +0200

    Fix output tiroallafune.c

--- BT2/Tiro alla fune/tiroallafune.c
+++ BT2/Tiro alla fune/tiroallafune.c
@@ -30,59 +30,68 @@ static void IsBest(int* vcurr, int* vbest, int* sum, int* elem, int* topValore, 
 
 }
 
-static bool IsValid(int* vcurr,size_t n,int e) {
+static bool IsValid(int* vcurr,int n,int e) {
 
-	for (size_t k = 0; k < n; k++) {
+	for (int k = 0; k < n; k++) {
 
 		if (vcurr[k] == e) {
 			return false;
-		}
-
-		return true;
+		}		
 
 	}
+	return true;
 }
 
-static void Print(int* vcurr, size_t n, int check, int*elem, size_t k) {
+static void Print(int* vcurr, int n, int check, int* elem, int k) {
+	
+	bool trovato = false;
+	int* vett2 = calloc(k, sizeof(int));
+	int l = 0;
 
-	if (check == 0) {
-		printf("{");
-
-
-		for (int i = 0; i < n - 2; i++) {
-			if (vcurr[i] != 0) {
-				printf(" %d,", vcurr[i]);
+	for (int i = 0; i < k; i++) {
+		trovato = false;
+		for (int j = 0; j < n; j++) {
+			if (elem[i] == vcurr[j]) {
+				trovato = true;
 			}
 		}
-
-		printf(" %d }, ", vcurr[n - 1]);
-
-		printf("{");
-		for (int i = 0; i < k-1; i++) {
-			bool trovato = false;
-			for (int j = 0; j < k / 2; j++) {
-				if (elem[i] == vcurr[j]) {
-					trovato = true;
-				}
-			}
-			if (!trovato) {
-				if (elem[i] != 0) {
-					printf(" %d", elem[i]);
-				}
-				if (i != k - 2) {
-					printf(",");
-				}
-			}
-
-
+		if (!trovato) {
+			vett2[l] = elem[i];
+			l++;
 		}
-		printf(" }");
 	}
-	else {
-		printf("{ }, { }");
+	
+	printf("{ ");
+	for (int i = 0; i < n; i++) {
+
+		if (i == n - 1) {
+			printf("%d", vcurr[i]);
+		}
+		else {
+			printf("%d, ", vcurr[i]);
+		}
+
 	}
+	printf(" }, ");
+
+	printf("{ ");
+	for (int i = 0; i < n + check; i++) {
+
+		if (i == (n + check) - 1) {
+			printf("%d", vett2[i]);
+		}
+		else {
+			printf("%d, ", vett2[i]);
+		}
+
+	}
+	printf(" }");
+
+
+	
+
 }
-static void TiroAllaFuneRec(int* vcurr, int* vbest, size_t i, size_t n, size_t k, int* elem, int* sum, int* topValore) {
+static void TiroAllaFuneRec(int* vcurr, int* vbest, int i, int n, int k, int* elem, int* sum, int* topValore) {
 
 	if (i == n) {
 		IsBest(vcurr, vbest, sum, elem,topValore,n,k);
@@ -90,7 +99,7 @@ static void TiroAllaFuneRec(int* vcurr, int* vbest, size_t i, size_t n, size_t k
 		return;
 	}
 
-	for (size_t l = 0; l < k; l++) {
+	for (int l = 0; l < k; l++) {
 
 		if (IsValid(vcurr, n,elem[l])) {
 			vcurr[i] = elem[l];
@@ -105,7 +114,7 @@ int main(int argc, char* argv[]) {
 	
 	char* endptr;
 	int* elem = calloc(argc - 1, sizeof(int));
-	size_t j = 0;
+	int j = 0;
 
 	for (int i = 1; i < argc; i++) {
 		elem[j] = strtol(argv[i], &endptr, 10);
@@ -115,12 +124,19 @@ int main(int argc, char* argv[]) {
 		j++;
 	}
 
-	int* vcurr = calloc(argc - 1, sizeof(size_t));
-	int* vbest = calloc(argc - 1, sizeof(size_t));
+	int* vcurr = calloc(argc - 1, sizeof(int));
+
+	int* vbest = calloc(argc - 1, sizeof(int));
+
 	int sum = 0;
 	int topValore = -1;
 	TiroAllaFuneRec(vcurr, vbest, 0, (argc - 1)/2,argc - 1, elem, &sum, &topValore);
-	Print(vbest, (argc - 1), 0, elem, (argc - 1 / 2));
+	if ((argc - 1) % 2 == 0) {
+		Print(vbest, (argc - 1) / 2, 0, elem, (argc - 1));
+	}
+	else {
+		Print(vbest, (argc - 1) / 2, 1, elem, (argc - 1));
+	}
 
 	free(elem);
 	free(vcurr);
